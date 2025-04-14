50545c5747a4695eb532b4de3ed5cc9b9a073c2a
GavinBright <gmbright42@tntech.edu>
2025-04-14 13:29:40 -0500

    Added a dynamic list of reviews that will sort them into pending or completed based on the placeholder

--- Student View/app.js
+++ Student View/app.js
@@ -20,8 +20,7 @@
                 data = await response.json();
 
                 loadAccountInfo()
-                // loadPendingReviews()
-                // loadCompletedReviews()
+                loadReviews()
             } catch (err){
                 console.log("Failed to load in data.")
             }
@@ -56,3 +55,56 @@
         }
         }
 
+        function loadReviews(){
+            const user = data.users.find(u => u.id === placeholderUserID) //Same code to validate if the account is real.
+            if(!user) return  //Once backend is added, might not even be used here.
+
+            const completedReviews = [] //Array for completed reviews.
+            const pendingReviews = [] //Array for pending reviews.
+
+            data.reviews.forEach(review => {
+                if(review.is_public === false){ //Check if it is public.
+                    return
+                }
+                //Below code will make it so user don't see reviews for courses they are not enrolled in.
+                const userEnrolled = data.enrolled.some(e => e.course_id === review.course_id && e.student_id === user.id) 
+                if(!userEnrolled){ 
+                    return
+                }
+                //If there is a matching entry in completed_review
+                const statusComplete = data.completed_review.find(c => c.review_id === review.id)
+                if(statusComplete){ //Add to completedReviews array if found.
+                    completedReviews.push(review)
+                }else { //If not, add to pending.
+                    pendingReviews.push(review)
+                }
+            })
+
+            displayReviewsStatus(completedReviews, 'completedReviewList', true); 
+            displayReviewsStatus(pendingReviews, 'pendingReviewList', false);
+        }
+
+        function displayReviewsStatus(reviews, listId, completed){ //Function to load data into the correct list.
+            const reviewList = document.getElementById(listId) //Should get the correct card.
+            reviewList.innerHTML = ''
+
+            if (reviews.length === 0){ //Create this element if blank.
+                const list = document.createElement("li");
+                list.classList.add("list-group-item");
+                list.textContent = "No reviews available.";
+                reviewList.appendChild(list);
+            } else {
+                reviews.forEach(review =>{
+                    const list = document.createElement("li");
+                    list.classList.add("list-group-item");
+                    if(completed){ //Don't need to display due date if completed.
+                        list.textContent = `${review.title}`;
+                    } else{
+                        list.textContent = `${review.title} (Due: ${review.due_at})`;
+                    }
+                    reviewList.appendChild(list);
+                })
+            }
+        }
+
+        //Displays pending regardless of account.
